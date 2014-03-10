5ab17d1f30cb1805c29de75d3fc1d2110b3457e2
Taylor Trimble <taylor@taylortrimble.com>
2014-03-09 22:23:30 -0500

    Average 10 trials, echo client

--- echo_client.go
+++ echo_client.go
@@ -16,30 +16,43 @@ func main() {
 		return
 	}
 
-	start := time.Now()
+	durations := make([]time.Duration, 10)
 
-	written, err := io.CopyN(conn, rand.Reader, 100)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	if written != 100 {
-		log.Println("Did not send 100 bytes")
-		return
+	for i := 0; i < 10; i++ {
+		start := time.Now()
+
+		written, err := io.CopyN(conn, rand.Reader, 100)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if written != 100 {
+			log.Println("Did not send 100 bytes")
+			return
+		}
+
+		response := make([]byte, 100)
+		received, err := conn.Read(response)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		var end time.Time
+		if received == 100 {
+			end = time.Now()
+		}
+
+		durations[i] = end.Sub(start)
+
+		log.Println("Took", durations[i])
 	}
 
-	response := make([]byte, 100)
-	received, err := conn.Read(response)
-	if err != nil {
-		log.Println(err)
-		return
+	var average int64 = 0
+	for _, d := range durations {
+		average += int64(d)
 	}
+	average /= int64(len(durations))
 
-	var end time.Time
-	if received == 100 {
-		end = time.Now()
-	}
-
-	log.Println(response)
-	log.Println("Took", end.Sub(start))
+	log.Println("Average:", time.Duration(average))
 }
