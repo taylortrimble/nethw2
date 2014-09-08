f7111380bd615a482552fd3493b74eb35c7d039b
Taylor Trimble <taylor@taylortrimble.com>
2014-09-07 21:57:52 -0700

    Whatever I did for homework

--- client.go
+++ client.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"time"
+	"strings"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "bulbasaur.tntapp.co:4949")
+	host, port, err := MyServer("trim0055", "apollo.cselabs.umn.edu:9080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", host, port))
 	if err != nil {
 		log.Println(err)
 		return
@@ -43,6 +51,112 @@ func main() {
 		}
 	}
 
-	log.Println(string(response))
-	log.Println("Took", end.Sub(start))
+	myIP := strings.Split(conn.LocalAddr().String(), ":")[0]
+	elapsed := int(end.Sub(start) / time.Millisecond)
+	err = ReportTime("trim0055", myIP, host, port, fmt.Sprintf("%d", elapsed), "apollo.cselabs.umn.edu:9090")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func MyServer(x500, endpoint string) (string, string, error) {
+	conn, err := net.Dial("tcp", endpoint)
+	if err != nil {
+		return "", "", err
+	}
+
+	message := fmt.Sprintf("serverlist\r\n")
+	fmt.Printf("> %s", message)
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		return "", "", err
+	}
+
+	received := make([]byte, 10000)
+	for err == nil {
+		_, err = conn.Read(received)
+	}
+
+	trimmed := bytes.TrimRight(received, "\n\x00")
+	split := bytes.Split(trimmed, []byte("\r"))
+
+	host := ""
+	port := ""
+	for _, server := range split[1:] {
+		parts := bytes.Split(server, []byte(" "))
+		if string(parts[0]) == "trim0055" {
+			host = string(parts[1])
+			port = string(parts[2])
+		}
+		fmt.Printf("< %s\n", server)
+	}
+
+	err = conn.Close()
+	if err != nil {
+		return "", "", err
+	}
+
+	return host, port, nil
+}
+
+func ReportTime(x500, clientIP, serverIP, serverPort, time, endpoint string) error {
+	message := fmt.Sprintf("setrecord %s %s %s %s %s\r\n", x500, clientIP, serverIP, serverPort, time)
+	fmt.Printf("> %s", message)
+	conn, err := net.Dial("tcp", endpoint)
+	if err != nil {
+		return err
+	}
+
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		return err
+	}
+
+	received := make([]byte, 20)
+	for err == nil {
+		_, err = conn.Read(received)
+	}
+
+	trimmed := bytes.TrimRight(received, "\r\n\x00")
+	fmt.Printf("< %s\n", trimmed)
+
+	err = conn.Close()
+	if err != nil {
+		return err
+	}
+
+	// Get List
+
+	conn, err = net.Dial("tcp", endpoint)
+	if err != nil {
+		return err
+	}
+
+	message = fmt.Sprintf("getrecord\r\n")
+	fmt.Printf("> %s", message)
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		return err
+	}
+
+	received = make([]byte, 10000)
+	for err == nil {
+		_, err = conn.Read(received)
+	}
+
+	trimmed = bytes.TrimRight(received, "\n\x00")
+	split := bytes.Split(trimmed, []byte("\r"))
+
+	for _, record := range split {
+		fmt.Printf("< %s\n", record)
+	}
+
+	// Done
+
+	err = conn.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- server.go
+++ server.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"strings"
+	"time"
 )
 
 func main() {
-	listen, err := net.Listen("tcp", ":4949")
+	ticker := time.NewTicker(35 * time.Second)
+	go func() {
+		for {
+			err := Register("trim0055", 9949, "apollo.cselabs.umn.edu:9080")
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			<-ticker.C
+		}
+	}()
+
+	listen, err := net.Listen("tcp", ":9949")
 	if err != nil {
 		log.Println(err)
 	}
@@ -43,4 +58,33 @@ func main() {
 			log.Println(err)
 		}
 	}
+}
+
+func Register(author string, port int, endpoint string) error {
+	conn, err := net.Dial("tcp", endpoint)
+	if err != nil {
+		return err
+	}
+
+	remoteIP := strings.Split(conn.LocalAddr().String(), ":")[0]
+	message := fmt.Sprintf("Register %s %s %d\r\n", author, remoteIP, port)
+	fmt.Printf("> %s", message)
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		return err
+	}
+
+	b := make([]byte, 20)
+	for err == nil {
+		_, err = conn.Read(b)
+	}
+
+	fmt.Printf("< %s", b)
+
+	err = conn.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
